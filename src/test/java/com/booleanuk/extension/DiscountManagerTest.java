3ad9a5bfa6bd1b2d78eccffebd154407b46feb20
karim <01171252@pw.edu.pl>
2023-07-13 11:47:04 +0200

    random changes

--- src/test/java/com/booleanuk/extension/DiscountManagerTest.java
+++ src/test/java/com/booleanuk/extension/DiscountManagerTest.java
@@ -15,30 +15,30 @@ public class DiscountManagerTest {
     public void testCalculateDiscountWith5Bagels() {
 
         for (int i = 0; i < 5; i++) {
-            basket.addBagel(ItemTypeEnum.BGLO);
+            basket.addItem(ItemTypeEnum.BGLO);
         }
 
-        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWith6Bagels() {
 
         for (int i = 0; i < 6; i++) {
-            basket.addBagel(ItemTypeEnum.BGLO);
+            basket.addItem(ItemTypeEnum.BGLO);
         }
 
-        Assertions.assertEquals(2.49, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(2.49, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWith7Bagels() {
 
         for (int i = 0; i < 7; i++) {
-            basket.addBagel(ItemTypeEnum.BGLO);
+            basket.addItem(ItemTypeEnum.BGLO);
         }
 
-        Assertions.assertEquals(2.49, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(2.49, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
@@ -47,10 +47,10 @@ public class DiscountManagerTest {
         basket.setCapacity(13);
 
         for (int i = 0; i < 13; i++) {
-            basket.addBagel(ItemTypeEnum.BGLO);
+            basket.addItem(ItemTypeEnum.BGLO);
         }
 
-        Assertions.assertEquals(4.98, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(4.98, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
@@ -59,10 +59,10 @@ public class DiscountManagerTest {
         basket.setCapacity(26);
 
         for (int i = 0; i < 24; i++) {
-            basket.addBagel(ItemTypeEnum.BGLO);
+            basket.addItem(ItemTypeEnum.BGLO);
         }
 
-        Assertions.assertEquals(9.96, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(9.96, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
@@ -71,61 +71,61 @@ public class DiscountManagerTest {
         basket.setCapacity(30);
 
         for (int i = 0; i < 24; i++) {
-            basket.addBagel(ItemTypeEnum.BGLO);
+            basket.addItem(ItemTypeEnum.BGLO);
         }
 
-        basket.addBagel(ItemTypeEnum.COFB);
-        basket.addBagel(ItemTypeEnum.BGLE);
+        basket.addItem(ItemTypeEnum.COFB);
+        basket.addItem(ItemTypeEnum.BGLE);
 
-        Assertions.assertEquals(11.21, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(11.21, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWithCofeAndBagel() {
 
-        basket.addBagel(ItemTypeEnum.COFB);
-        basket.addBagel(ItemTypeEnum.BGLE);
+        basket.addItem(ItemTypeEnum.COFB);
+        basket.addItem(ItemTypeEnum.BGLE);
 
-        Assertions.assertEquals(1.25, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(1.25, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWith2CofeAndBagel3() {
 
-        basket.addBagel(ItemTypeEnum.COFB);
-        basket.addBagel(ItemTypeEnum.COFB);
-        basket.addBagel(ItemTypeEnum.BGLE);
-        basket.addBagel(ItemTypeEnum.BGLE);
-        basket.addBagel(ItemTypeEnum.BGLE);
+        basket.addItem(ItemTypeEnum.COFB);
+        basket.addItem(ItemTypeEnum.COFB);
+        basket.addItem(ItemTypeEnum.BGLE);
+        basket.addItem(ItemTypeEnum.BGLE);
+        basket.addItem(ItemTypeEnum.BGLE);
 
-        Assertions.assertEquals(2.50, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(2.50, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWithNoItems() {
-        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getList()));
+        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWithOneCoffee() {
-        basket.addBagel(ItemTypeEnum.COFB);
-        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getList()));
+        basket.addItem(ItemTypeEnum.COFB);
+        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWithMultipleCoffees() {
-        basket.addBagel(ItemTypeEnum.COFB);
-        basket.addBagel(ItemTypeEnum.COFB);
-        basket.addBagel(ItemTypeEnum.COFB);
-        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getList()));
+        basket.addItem(ItemTypeEnum.COFB);
+        basket.addItem(ItemTypeEnum.COFB);
+        basket.addItem(ItemTypeEnum.COFB);
+        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getBasket()));
     }
 
     @Test
     public void testCalculateDiscountWithDifferentBagelTypes() {
-        basket.addBagel(ItemTypeEnum.BGLO);
-        basket.addBagel(ItemTypeEnum.BGLP);
-        basket.addBagel(ItemTypeEnum.BGLE);
-        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getList()));
+        basket.addItem(ItemTypeEnum.BGLO);
+        basket.addItem(ItemTypeEnum.BGLP);
+        basket.addItem(ItemTypeEnum.BGLE);
+        Assertions.assertEquals(0, discountManager.calculateDiscount(basket.getBasket()));
     }
 
 }
