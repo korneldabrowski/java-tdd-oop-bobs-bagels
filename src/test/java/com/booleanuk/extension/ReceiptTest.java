9191988969b0fdf0a817d9ca4c70e0781ef1ec3c
Kornel <korneldabrowski@gmail.com>
2023-07-13 15:31:32 +0200

    added sysout to print out discounted receipt

--- src/test/java/com/booleanuk/extension/ReceiptTest.java
+++ src/test/java/com/booleanuk/extension/ReceiptTest.java
@@ -3,18 +3,19 @@ package com.booleanuk.extension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class ReceiptTest {
     @Test
-    public void receiptTest(){
-               Basket basket = new Basket();
+    public void receiptTest() {
+        Basket basket = new Basket();
         basket.setCapacity(30);
         basket.addItem(ItemTypeEnum.BGLO, 2);
         basket.addItem(ItemTypeEnum.BGLP, 12);
         basket.addItem(ItemTypeEnum.BGLE, 6);
         basket.addItem(ItemTypeEnum.COFB, 3);
         String actualReceipt = basket.normalReceipt(basket);
+
         System.out.println(actualReceipt);
+
         Assertions.assertTrue(actualReceipt.contains("Onion Bagel         2   $0.98"));
         Assertions.assertTrue(actualReceipt.contains("Plain Bagel        12   $4.68"));
         Assertions.assertTrue(actualReceipt.contains("Everything Bagel    6   $2.94"));
@@ -22,14 +23,17 @@ public class ReceiptTest {
     }
 
     @Test
-    public void receiptTestWithDiscount(){
+    public void receiptTestWithDiscount() {
         Basket basket = new Basket();
         basket.setCapacity(30);
         basket.addItem(ItemTypeEnum.BGLO, 2);
         basket.addItem(ItemTypeEnum.BGLP, 12);
         basket.addItem(ItemTypeEnum.BGLE, 6);
         basket.addItem(ItemTypeEnum.COFB, 3);
+
         String actualReceipt = basket.receiptWithDiscount(basket);
+
+        System.out.println(actualReceipt);
         Assertions.assertTrue(actualReceipt.contains("Onion Bagel         2   $0.98"));
         Assertions.assertTrue(actualReceipt.contains("Plain Bagel        12   $4.68"));
         Assertions.assertTrue(actualReceipt.contains("                     (-$0.45)"));
