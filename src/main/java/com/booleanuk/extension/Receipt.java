43b7a54f70e2a7ad399ba09a00ac03bcf976a87f
karim <01171252@pw.edu.pl>
2023-07-13 13:59:06 +0200

    receipt with discount

--- src/main/java/com/booleanuk/extension/Receipt.java
+++ src/main/java/com/booleanuk/extension/Receipt.java
@@ -2,6 +2,7 @@ package com.booleanuk.extension;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 public interface Receipt {
     default String normalReceipt(Basket basket){
@@ -19,7 +20,6 @@ public interface Receipt {
             String itemName = item.getType().getVariant() + " " + item.getType().getName();
             int count = item.getCount();
             double itemCost = item.getType().getPrice();
-            //TODO ten normalny koszt
             double itemTotalCost = count * itemCost;
             receiptBuilder.append(String.format("%-18s %2d   $%.2f\n", itemName, count, itemTotalCost));
         }
@@ -31,4 +31,54 @@ public interface Receipt {
 
         return receiptBuilder.toString();
     }
+
+    default String receiptWithDiscount(Basket basket) {
+        DiscountManager discountManager = new DiscountManager();
+        discountManager.calculateDiscount(basket);
+        HashMap<Item, Double> finalPrize = discountManager.getFinalPrize();
+        StringBuilder receiptBuilder = new StringBuilder();
+
+        receiptBuilder.append("    ~~~ Bob's Bagels ~~~\n\n");
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        receiptBuilder.append(now.format(formatter)).append("\n\n");
+
+        receiptBuilder.append("----------------------------\n\n");
+
+        double totalSavings = 0.0;
+
+        for (Item item : basket.getBasket()) {
+            String itemName = item.getType().getVariant() + " " + item.getType().getName();
+            int count = item.getCount();
+            double itemCost = item.getType().getPrice();
+            double itemTotalCost = count * itemCost;
+
+            System.out.println(finalPrize);
+            System.out.println(item);
+
+            double discountAmount = 0.0;
+            if (finalPrize.containsKey(item)){
+            discountAmount = item.getCount() * item.getType().getPrice() - finalPrize.get(item);}
+
+            if (discountAmount > 0.0) {
+                receiptBuilder.append(String.format("%-18s %2d   $%.2f\n", itemName, count, itemTotalCost));
+                receiptBuilder.append(String.format("%-20s (-$%.2f)\n", "", discountAmount));
+                totalSavings += discountAmount;
+            } else {
+                receiptBuilder.append(String.format("%-18s %2d   $%.2f\n", itemName, count, itemTotalCost));
+            }
+        }
+
+        receiptBuilder.append("\n----------------------------\n\n");
+        receiptBuilder.append(String.format("Total                 $%.2f\n\n", basket.getTotalCost() - totalSavings));
+        receiptBuilder.append(String.format(" You saved a total of $%.2f\n", totalSavings));
+        receiptBuilder.append("       on this shop\n\n");
+        receiptBuilder.append("        Thank you\n");
+        receiptBuilder.append("      for your order!");
+
+        return receiptBuilder.toString();
+    }
+
+
 }
--- src/test/java/com/booleanuk/extension/ReceiptTest.java
+++ src/test/java/com/booleanuk/extension/ReceiptTest.java
@@ -20,4 +20,19 @@ public class ReceiptTest {
         Assertions.assertTrue(actualReceipt.contains("Everything Bagel    6   $2.94"));
         Assertions.assertTrue(actualReceipt.contains("Black Coffee        3   $2.97"));
     }
+
+    @Test
+    public void receiptTestWithDiscount(){
+        Basket basket = new Basket();
+        basket.setCapacity(30);
+        basket.addItem(ItemTypeEnum.BGLO, 2);
+        basket.addItem(ItemTypeEnum.BGLP, 12);
+        basket.addItem(ItemTypeEnum.BGLE, 6);
+        basket.addItem(ItemTypeEnum.COFB, 3);
+        String actualReceipt = basket.receiptWithDiscount(basket);
+        Assertions.assertTrue(actualReceipt.contains("Onion Bagel         2   $0.98"));
+        Assertions.assertTrue(actualReceipt.contains("Plain Bagel        12   $4.68"));
+        Assertions.assertTrue(actualReceipt.contains("                     (-$0.45)"));
+        Assertions.assertTrue(actualReceipt.contains("Total                 $10.43"));
+    }
 }
